dca7b2994e8192606eb5246b3c397e3b4fcaf007
gogpg <giedre.luko@gmail.com>
2022-06-10 19:23:44 +0300

    server

---
+++ index.js
@@ -0,0 +1,27 @@
+ import { server } from './lib/server.js'
+//import { utils } from './lib/utils.js';
+ 
+
+const app = {};
+
+app.init = () => {
+    console.log('inicijuojama programa...')
+      // susikurti busimus reikiamus folder'ius
+    // susikurti busimus reikiamus failus, pvz.: robots.txt
+
+    // prisijungti prie duomenu bazes (DB)
+
+    // paleidziam pacio serverio logika (perduodame prisijungima prie DB)
+    server.init();
+
+    // paleidzia papildomus nuolatinius procesus:
+    // - issitrinti nebereikalingus failus
+    // - suarchivuoti retai naudojamus failus
+    // - prasukti reikiamus API (3rd-party info, pvz.: valiutu kursai, orai)
+    // - pasitikrinti, jog vartotojai, kurie neturi teises buti prisijunge, jog ir nera prisijunge, o jei yra - atjungiame!!!
+}
+
+
+app.init();
+
+export default app;
