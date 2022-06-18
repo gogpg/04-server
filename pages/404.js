9ec14363ff52a5fd327bc95aebae89d1d401c0d0
gogpg <giedre.luko@gmail.com>
2022-06-18 21:57:15 +0300

    Page class update

--- pages/404.js
+++ pages/404.js
@@ -1,7 +1,16 @@
 import { PageTemplate } from "../lib/PageTemplate.js";
 
 class Page404 extends PageTemplate {
- 
+    constructor() {
+        super();
+        this.title = '404 | Server';
+        
+       
+    }
+
+    mainHTML() {
+        return `<h1>404 page 🎅</h1>`
+    }
 
 }
 
--- pages/home.js
+++ pages/home.js
@@ -2,7 +2,14 @@ import { PageTemplate } from "../lib/PageTemplate.js";
 
 
 class PageHome extends PageTemplate {
-  
+    constructor() {
+        super();
+        
+    }
+
+    mainHTML() {
+        return `<h1>Home page 🎅</h1>`
+    }
 }
 
 export { PageHome };
