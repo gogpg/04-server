bb5c4e7d19e908a73d500a35e36b11b6133d4368
gogpg <giedre.luko@gmail.com>
2022-06-23 11:35:02 +0300

    IsValid email logic and tests

--- public/js/components/IsValid.js
+++ public/js/components/IsValid.js
@@ -2,28 +2,29 @@
 
 class IsValid {
     static fullname(str) {
-        if (str === undefined) {
+        if (str === undefined) {    ///jei nieko neivesta
             return [true, 'Neduotas parametras'];
         }
         if (typeof str !== 'string') {
             return [true, 'Netinkamas tipas, turi buti "string"'];
         }
 
-        str = str.trim().replace(/\s+/g, ' ');
-        // str = str.trim().replaceAll('  ', ' '); // galimas ir sitas variantas
+        str = str.trim().replace(/\s+/g, ' ');  //pakeicia visus didesnius tarpus i viena tarpa viduje stringo, pries tai su trim panaikinus tuscias vietas is abieju pusiu.
+        // str = str.trim().replaceAll('  ', ' '); // galimas ir toks budas.
 
         const minWordsCount = 2;
         const minWordLength = 2;
         const minTextLength = minWordsCount * minWordLength + (minWordsCount - 1);
+
         if (str.length < minTextLength) {
             return [true, `Per trumpas tekstas, turi buti minimum ${minTextLength} simboliai`];
         }
 
         const allowedSymbols = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-
         const words = str.split(' ');
+
         if (words.length < minWordsCount) {
-            return [true, `Tekstas turi tureti ${minWordsCount} arba daugiau zodziu`];
+            return [true, `Tekstas turi tureti ${minWordsCount} arba daigiau zodziu`];
         }
         for (const word of words) {
             if (word.length < minWordLength) {
@@ -53,16 +54,70 @@ class IsValid {
     }
 
     static email(str) {
-        if (str.length < 2) {
-            return [true, 'Per trumpas email tekstas'];
+        if (typeof str !== 'string') {    ////jei ivesta reiksme nera stringas.
+            return [true, 'Netinkamas tipas, turi buti "string"'];  //rasta klaida. true.
+        }
+        str = str.trim();
+        if (str === '') {
+            return [true, 'Neivestas email adresas'];
+        }
+
+        const parts = str.split('@');  
+        if (parts.length !== 2) {  //jeigu gautu nariu ilgis nera du (jei yra vienas @ bus dvi dalys splitinus, jei keli @ tai bus daugiau daliu, jei nera @ tai nesisplitins, taip ir liks tik viena dalis)
+            return [true, 'El pasto adresas privalo tureti tik viena @ simboli'];
+        }
+
+        const [locale, domain] = parts;  //locale - email dalis iki @, domen - uz @ simbolio.
+        if (locale === '') {
+            return [true, 'Truksta dalies pries @ simboli'];
+        }
+        if (domain === '') {
+            return [true, 'Truksta dalies uz @ simboli'];
+        }
+
+        if (str.includes('..')) {
+            return [true, 'El pastas negali tureti dvieju tasku is eiles'];
+        }
+
+        const allowedSymbols = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.';
+
+        if (locale[0] === '.'      //negali prasideti tasku ir negali prasideti skaiciumi
+            || !isNaN(+locale[0])) {
+            return [true, 'El pastas turi prasideti raide'];
+        }
+        for (const symbol of locale) {
+            if (!allowedSymbols.includes(symbol)) {
+                return [true, `Pries @ neleistinas naudoti simbolis "${symbol}"`];
+            }
+        }
+
+        const domainParts = domain.split('.');  //splitinam domaina per taska arba taskus, jei ju daugiau.
+        if (domainParts.length === 1) {
+            return [true, 'Uz @ simbolio truksta tasko (netinkamas domenas)'];
+        }
+        if (domainParts[0] === '') {
+            return [true, `Uz @ dalies tekstas negali prasideti tasku`];
+        }
+        if (domainParts[domainParts.length - 1].length < 2) {  //domain dalyje, kreipiames i paskutini nari, tikrinu koks jo ilgis. Domain yra pries tai issplitintas per taska arba taskus.
+            return [true, `Uz @ dalies domenas turi baigtis bent dviejomis raidemis`];
+        }
+        for (const symbol of domain) {
+            if (!allowedSymbols.includes(symbol)) {
+                return [true, `Uz @ neleistinas naudoti simbolis "${symbol}"`];
+            }
         }
 
         return [false, 'OK'];
     }
 
     static password(str) {
-        if (str.length < 2) {
-            return [true, 'Per trumpas password tekstas'];
+        const minPasswordLength = 12;
+
+        if (typeof str !== 'string') {
+            return [true, 'Netinkamas tipas, turi buti "string"'];
+        }
+        if (str.length < minPasswordLength) {
+            return [true, `Per trumpas password tekstas, turi buti minimum ${minPasswordLength} simboliai`];
         }
 
         return [false, 'OK'];
